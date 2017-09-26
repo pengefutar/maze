0b4a0b032912b45bbe7bbbb3a91f2e9ba6624d50
pengefutar <lukacseszterjudit@gmail.com>
2017-09-26 13:13:03 +0200

    jaxafx gui, new maze generating and exiting with popup window done, cheat has bugs

--- src/com/codecool/maze/ConsolePrinter.java
+++ src/com/codecool/maze/ConsolePrinter.java
@@ -4,6 +4,17 @@ import java.util.Arrays;
 
 public class MazeConsolePrinter {
 
+    public static final String yellow = "\u001B[33m";
+    public static final String blue = "\u001B[36m";
+    public static final String reset = "\033[0m";
+
+    public static void main(String[] args) {
+        Maze mazeGenerator = new Maze(21, 31);
+        PathFinder maze = mazeGenerator.generate();
+        MazeConsolePrinter.printWithoutSolution(maze);
+        MazeConsolePrinter.printWithSolution(maze);
+    }
+
     private static boolean isListContainingElement(int[][] list, int[] element) {
         for (int i = 0; i < list.length; i++) {
             if (Arrays.equals(list[i], element)) {
@@ -36,10 +47,10 @@ public class MazeConsolePrinter {
         for (int row = 0; row < maze.getMaze().length; row++) {
             for (int cell = 0; cell < maze.getMaze()[0].length; cell++) {
                 if (maze.getMaze()[row][cell] == 1) {
-                    System.out.print("x ");
+                    System.out.print(blue + "x " + reset);
                 } else if (maze.getMaze()[row][cell] == 0) {
                     if (isListContainingElement(shortestPathCells, new int[]{row, cell})) {
-                        System.out.print("o ");
+                        System.out.print(yellow + "o " + reset);
                     } else {
                         System.out.print("  ");
                     }
